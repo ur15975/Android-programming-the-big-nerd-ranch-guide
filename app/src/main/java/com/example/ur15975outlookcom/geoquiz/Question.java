66399abd6149473e3eb4d5441627003992dc0f96
Che Lixi <ur15975@gmail.com>
2017-03-10 17:28:14 +0800

    第2 章 Android 与MVC 设计模式

---
+++ app/src/main/java/com/example/ur15975outlookcom/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.example.ur15975outlookcom.geoquiz;
+
+/**
+ * Created by ur15975@outlook.com on 2017/3/10.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
