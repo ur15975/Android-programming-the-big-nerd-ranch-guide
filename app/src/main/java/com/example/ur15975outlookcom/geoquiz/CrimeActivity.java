6a5d7ad900adf65aff942c9f8f0a09dfc16a2bd4
Che Lixi <ur15975@gmail.com>
2017-03-19 12:57:32 +0800

    第10 章 使用fragment argument

--- app/src/main/java/com/example/ur15975outlookcom/geoquiz/CrimeActivity.java
+++ app/src/main/java/com/example/ur15975outlookcom/geoquiz/CrimeActivity.java
@@ -1,16 +1,30 @@
 package com.example.ur15975outlookcom.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.widget.Button;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
 
+    private static final String EXTRA_CRIME_ID =
+            "com.bignerdranch.android.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeID) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeID);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
--- app/src/main/java/com/example/ur15975outlookcom/geoquiz/CrimeFragment.java
+++ app/src/main/java/com/example/ur15975outlookcom/geoquiz/CrimeFragment.java
@@ -12,30 +12,44 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.UUID;
+
 /**
  * Created by ur15975@outlook.com on 2017/3/17.
  */
 
 public class CrimeFragment extends Fragment {
+    private static final String ARG_CRIME_ID = "crime_id";
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
 
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
-
+        UUID crimeId = (UUID) getArguments()
+                .getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
 
         mTitleField = (EditText) view.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -58,6 +72,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);//禁用按钮,可以不响应用户单击事件,样式编程灰色
 
         mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);//监控CheckBox是否改变
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
